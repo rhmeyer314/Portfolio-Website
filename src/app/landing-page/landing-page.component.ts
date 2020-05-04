e953cdd69660778210f0bb04458b7ff9be50f7ff
rhmeyer314 <rhmeyer314@gmail.com>
2020-05-04 02:44:11 -0400

    added first icon

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+  faHome = faHome;
 
   constructor() { }
 
