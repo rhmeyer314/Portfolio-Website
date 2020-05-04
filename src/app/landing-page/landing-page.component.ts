c9ed3f4dae38c0ec7f61961734bc70e2db633172
rhmeyer314 <rhmeyer314@gmail.com>
2020-05-04 14:26:38 -0400

    imported more font awesome icons

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faTh } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,6 +12,11 @@ import { faHome } from '@fortawesome/free-solid-svg-icons';
 })
 export class LandingPageComponent implements OnInit {
   faHome = faHome;
+  faTh = faTh;
+  faUser = faUser;
+  faFile = faFile;
+  faEnvelope = faEnvelope;
+
 
   constructor() { }
 
