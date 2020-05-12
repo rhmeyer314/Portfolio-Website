f613a7ebdf83192160dceb28430ec80f2d734d27
rhmeyer314 <rhmeyer314@gmail.com>
2020-05-12 18:01:58 -0400

    added createProjects() function

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -8,6 +8,7 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import * as $ from 'jquery';
+import { Projects } from "../model/projects"
 
 @Component({
   selector: 'app-landing-page',
@@ -24,11 +25,17 @@ export class LandingPageComponent implements OnInit {
   faGithub = faGithub;
   faLinkedin = faLinkedin;
   wasClicked = false;
+  projectsArray: Array<Projects>
 
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.projectsArray = this.createProjects();
+    console.log(this.projectsArray)
+
+
     $(window).on('resize', () => {
       this.wasClicked = this.wasClicked;
       var win = $(window);
@@ -66,5 +73,38 @@ export class LandingPageComponent implements OnInit {
   onClick(): void {
     this.wasClicked = !this.wasClicked;
     console.log(this.wasClicked)
+  }
+
+  createProjects(): Array<Projects> {
+    let projectsArray = new Array<Projects>();
+
+    let projects = new Projects();
+    projects.imageURL = "/assets/emuLadderBackground.png";
+    projects.title = "emuLadder";
+    projects.description = "emuLadder is a fantasy esports game similar to Fantasy Football, Hockey, etc. emuLadder was built using Angular, Spring Boot, NodeJS, and MySQL.";
+    projects.type = "Senior Capstone";
+    projects.projectURL = "https://github.com/CapstoneUNCG2020/emuLadder";
+
+    projectsArray.push(projects);
+
+    projects = new Projects();
+    projects.imageURL = "/assets/books.jpg";
+    projects.title = "ReadingLogger";
+    projects.description = "ReadingLogger is a fun way to keep track of interesting data from the books that you have read. ReadingLogger was built with Django/Python.";
+    projects.type = "Personal Project";
+    projects.projectURL = "https://github.com/rhmeyer314/ReadingLogger";
+
+    projectsArray.push(projects);
+
+    projects = new Projects();
+    projects.imageURL = "/assets/task.jpg";
+    projects.title = "SMS Reminder";
+    projects.description = "SMS Reminder is a web app where you can create a task list and recieve text messages as a reminder. SMS Reminder was built using Spring Boot/Java and the Twilio API.";
+    projects.type = "Personal Project";
+    projects.projectURL = "https://github.com/rhmeyer314/SmsReminder";
+
+    projectsArray.push(projects);
+
+    return projectsArray;
   }
 }
