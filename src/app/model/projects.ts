1805879e5115c63d76348dca69aba425215e4076
rhmeyer314 <rhmeyer314@gmail.com>
2020-05-12 18:02:25 -0400

    created and modifed projects.ts

---
+++ src/app/model/projects.ts
@@ -0,0 +1,26 @@
+export class Projects {
+    /**
+     * image url
+     */
+    imageURL: string;
+
+    /**
+     * title of the project
+     */
+    title: string;
+
+    /**
+     * the type of project, such as a "Personal Project" or "Senior Capstone"
+     */
+    type: string;
+
+    /**
+     * the description of the project
+     */
+    description: string;
+
+    /**
+     * github url to the project
+     */
+    projectURL: string;
+}
